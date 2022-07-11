a2bfdcf328cbfc2bdc5a84d3566172a7479cd310
mod64bits <mod64bits@gmail.com>
2022-07-11 14:24:21 -0300

    Home DashBoard

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from apps.products.models import TypeProduct, Product
 
 
+class DashBoardView(TemplateView):
+    template_name = 'core/dashBoard.html'
