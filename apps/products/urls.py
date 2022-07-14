a06ed0d152da90de7410a96513f0244570966aac
mod64bits <mod64bits@gmail.com>
2022-07-14 11:04:45 -0300

    parcial imagens

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, re_path
-from .views import ProductListView, ProductCreateView, ListTypeProductView, CreateTypeProductView
+from .views import (ProductListView, ProductCreateView, ListTypeProductView, CreateTypeProductView, UpdateProductView,
+                    UploadImageProductWiew)
 from apps.core.views import DashBoardView
 
 app_name = "products"
 
 urlpatterns = [
+    path('produto/<int:pk>/', UploadImageProductWiew.as_view(), name='products_image_update'),
+    path('editar/<slug:slug>/', UpdateProductView.as_view(), name='products_update'),
     path('novo/', ProductCreateView.as_view(), name='products_create'),
     path('lista/', ProductListView.as_view(), name='products_list'),
     path('tipo/', ListTypeProductView.as_view(), name='product_type_list'),
